98338ce8415c290823e4c74fa12f093db50aebfa
Florent Garnier <florent.garnier@imag.fr>
2011-05-05 17:08:59 +0200

    Adding plugin source files in the backup branch.

---
+++ src/SSL.ml
@@ -0,0 +1,16 @@
+(**************************************************************************************)
+
+
+(* This file describe the syntax and the functions required to manipulate Simple Separation
+Logic formulae *)
+
+
+
+
+(**************************************************************************************)
+
+
+
+
+open flata_language
+open intermediate_language
