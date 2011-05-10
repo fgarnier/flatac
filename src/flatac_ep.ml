9f25a47f995f1a97f8b6f5731282ba8bfa741a90
Maxime Gaudin <gaudin.maxime@gmail.com>
2011-05-10 14:20:43 +0200

    Mise en place du plugin de base

---
+++ src/flatac_ep.ml
@@ -0,0 +1,20 @@
+module Self =
+	Plugin.Register
+		(struct
+			let name = "FlataC"
+			let shortname = "FlataC"
+			let help= "This plugin demonstrate the work of Radu Iosif, Florent Ganier, Marius Bozga and Maxime Gaudin about the automatic verification on critical embedded softwares."
+		end)
+
+module Enabled =
+	Self.False
+		(struct
+	 		let option_name= "-flatac"
+			let help= ""
+			let kind= `Correctness
+		 end)
+
+let print () = Self.result "Hello world!"
+
+let run () = if Enabled.get () then print ()
+let () = Db.Main.extend run
--- src/logging.ml
+++
@@ -1,8 +0,0 @@
-module Logging
-	struct
-		(** This function register and display on output channel oc 
-		 a failure described by information message 'm' **)
-		let register_failure ( oc : out_channel ) ( m : string ) =
-    			Printf.printf "Failure"
-		;;
-	end;;
