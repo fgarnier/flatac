6128bd14c5b6678df5797c961b6aa1d51a39c935
Florent Garnier <florent.garnier@imag.fr>
2011-05-12 13:35:54 +0200

    Generating html doc

---
+++ src/SSL/SSL_types.ml
@@ -0,0 +1,12 @@
+open List
+
+module SSL_types = struct
+  type ptvar = PVar of string
+  type locvar = LVar of string
+  type eq = Eqloc of locvar * locvar
+  type affect = Pointsto of ptvar * locvar
+  type affectnil = Pointsnil of ptvar
+  type exists_loc = Exists of locvar
+  type pure_formula = Pure of eq list * affect list * affectnil list
+
+end;;
