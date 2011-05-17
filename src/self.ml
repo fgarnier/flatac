b0a7da974cbf1d54007609d05541f4b983854a29
Maxime Gaudin <gaudin.maxime@gmail.com>
2011-05-17 15:27:51 +0200

    Ajout du déportement du module Self

---
+++ src/self.ml
@@ -0,0 +1,8 @@
+module Self =
+	Plugin.Register
+		(struct
+			let name = "FlataC"
+			let shortname = "FlataC"
+			let help= "This plugin demonstrate the work of Radu Iosif, Florent Ganier, Marius Bozga \
+			and Maxime Gaudin about the automatic verification on critical embedded softwares."
+		end)
