d0d467d29fa831005a1d095e3a7c511a67facf6d
Maxime Gaudin <gaudin.maxime@gmail.com>
2011-05-17 16:18:01 +0200

    Et en plus j'oublie des ficheirs

---
+++ src/semAndLogicFrontEnd.ml
@@ -0,0 +1,10 @@
+open Self
+open Cil_types
+
+class virtual ['a] semAndLogicFrontEnd = 
+object 
+	method virtual getEntryPointAbstraction : unit -> 'a
+	method virtual getEntryPointPrecondition : unit -> 'a
+	method virtual isErrorState: 'a -> bool
+	method virtual next : 'a -> string -> stmtkind -> 'a 
+end
---
+++ src/trueLogicFrontEnd.ml
@@ -0,0 +1,12 @@
+open Self
+open Cil_types
+open SemAndLogicFrontEnd
+
+class trueLogicFrontEnd = 
+object 
+inherit [bool] semAndLogicFrontEnd
+	method getEntryPointAbstraction () = true
+	method getEntryPointPrecondition () = true 
+	method isErrorState state = (state = false) 
+	method next currentAbstraction guard stmtData =currentAbstraction 
+end
