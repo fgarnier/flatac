75d83847d26cfb9beacde340170889c278fc2bc7
Maxime Gaudin <gaudin.maxime@gmail.com>
2011-05-17 12:40:19 +0200

    Module eCFG terminé

--- src/ecfg.ml
+++ src/ecfg.ml
@@ -3,10 +3,39 @@ open Cil_types
 open Cfg
 open Visitor
 
+(*
+	Maxime Gaudin - VERIMAG 2011 
+
+	** THIS MODULE IS A PART OF FLATA-C, DEVELOPED AT VERIMAG (2011)
+
+	This module contains the definition of an eCFG and implements a generic algorithm to
+	fill it with the correct Abstract Interpretation and counter automata label
+
+	For any question mail us to :
+	- maxime.gaudin@imag.fr
+	- florent.garnier@imag.fr
+*)
+
+(** This HashTable is defined to avoir looping through CFG *)
+module HashInt =
+struct
+	type t = int
+	let equal i j = ( i = j )
+	let hash k = k
+end;;
+module IntHashtbl = Hashtbl.Make ( HashInt )
+
 module Ecfg = functor ( A : sig type t end ) ->
 struct
-	type cfgLeaf = Leaf of int * A.t
-	type cfg = Node of cfgLeaf * (cfgLeaf list)
+	type stmtId = int
+
+	type transitionOp = Op of Cil_types.stmtkind
+	| EntryPoint
+
+	type semanticValue = ASem of A.t 
+	| None
+
+	type cfg = Node of stmtId * transitionOp * semanticValue * (cfg list) 
 	| Empty
 
 	(* Container class of eCFG *)
@@ -23,14 +52,26 @@ struct
 		method getRoot () = _root
 	end
 
-	(* eCFG accessor. One eCFG exists for each global function *)
+	let rec _buildCfg ( stmtData : Cil_types.stmt ) visited = 
+		let children = ref [] in
+			List.iter 	( fun stmt -> 
+						try (** Already visited *)
+							if IntHashtbl.find visited stmt.sid then () 		
+							(** Dirty exception hacking to handle the "Already visited" case *)
+						with _ -> (** Never visited *)
+								IntHashtbl.add visited stmt.sid true;
+								children := (_buildCfg stmt visited) :: !children 
+					) stmtData.succs;
+			Node ( stmtData.sid, EntryPoint, None , !children ) 
+
+	(** Private method called by the CilCFG Visitor at each function *)
+	let buildCfg ( funInfo : fundec ) = new eCFG funInfo.svar.vname (_buildCfg (List.hd funInfo.sallstmts) (IntHashtbl.create 1))
+
+	(** eCFGs accessor. *)
 	let eCFGs : ( (eCFG list) Pervasives.ref ) = ref []
 
-	(* Private method called by the CilCFG Visitor at each function *)
-	let buildCfg ( funInfo : fundec ) = new eCFG funInfo.svar.vname Empty
-
-	(* This CfgVisitor visits each global function and trigger the build 
-	 of a new Cfg *)
+	(** This visitor visits global function and trigger the build 
+	 of a new Cfg each one *)
 	class cfgVisitor ( prj : Project.t ) 
 	= object(self)
 	inherit Visitor.generic_frama_c_visitor (prj) (Cil.inplace_visit())
@@ -43,6 +84,7 @@ struct
 			| _ -> DoChildren
 	end
 
+	(** Compute the eCFG and fill the structures *)
 	let computeECFGs ( prj : Project.t ) ( ast : Cil_types.file ) = 
 		let cfgVisitorInst = new cfgVisitor ( prj ) in	
 			visitFramacFile ( cfgVisitorInst :> frama_c_copy ) ast;
