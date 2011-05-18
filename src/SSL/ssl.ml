159d97ff5939f0513bd6ca5b739b3d36b00d9fcc
Florent Garnier <florent.garnier@imag.fr>
2011-05-18 19:08:12 +0200

    plpo

--- src/SSL/ssl.ml
+++ src/SSL/ssl.ml
@@ -1,3 +1,11 @@
+(** This files contains the operation use to create, normalize, and check 
+properties of SSL formulae
+
+ For questions, comment or any improvement proposal, please contact
+ florent<dot>garnier\\at//imag/dot\fr or gaudin.maxime\\at//gmail-DoT-fr.
+
+*)
+
 open List
 open Ssl_types
 open Ssl_types.SSL_lex
@@ -18,7 +26,6 @@ module SSL = struct
   let orient_eqlist   eqlist   =
     List.map ( fun s -> ( orient_eq s ) ) eqlist 
 
-   
 
   let fold_max (lvari : SSL_lex.locvar)()(lvinit : SSL_lex.locvar ) =
     match (lvari , lvinit ) with 
@@ -27,6 +34,8 @@ module SSL = struct
 	else LVar (y)
 
 
+
+    
   (* Returns the biggest key of hash table which keys are varlocs*)
   (* Any variable name in C contains at least a character, therefore
      is bigger than "".
@@ -35,12 +44,18 @@ module SSL = struct
   let biggest_loc_var ( tble : (( locvar , unit ) t) ) =
     Hashtbl.fold fold_max tble ( LVar("") )
  
- (*
-  let unif_loc_vars_iterande ( x : ptvar  ) =
-    unif_loc_vars ( puref : SSL_lex.pure_formula ) =
- *) 
+
+  (** 
+      
+   *)
+
+   (** *)
+  let unify_eq ( tble : ((locvar , unit )t )) =
+    
 
 
+
+ (* Called by del_tautologies. Shall not appear in the Interface. *)
 
   let rec _del_tautologies ( lg : eq list )( ld : eq list) =
     match lg , ld  with 
@@ -50,13 +65,18 @@ module SSL = struct
 	  then _del_tautologies x ld'
 	  else _del_tautologies ( Eqloc(LVar(g),LVar(d))::x) ld'
 
+
+ (** This fuction is used to remove trivial equalities, such as l1=l1*)
 	 
 
   let del_tautologies (l : eq list ) =
       _del_tautologies [] l
 
+(*   The part of the file that follows contains the functions that
+     encodes the substitutions of the formulae.
+*)
 
-
+ (* Not for the interface. Called by subst_eqlist*)
   let subst_loc (xv : locvar)(yv : locvar)( equality : eq) =
    match xv , yv with
     (LVar(x),LVar(y)) ->
@@ -70,14 +90,21 @@ module SSL = struct
 	      if ( SSL_lex.equals_to x b ) then Eqloc (LVar(a),LVar (y))
 	   else Eqloc ( LVar (a) , LVar (b) )
 	          
+ (** Use this to replace all instance of xv by yv in list lst. 
+Shall appear in the interface file. *)
 
   let subst_eqlist (xv : locvar) (yv :locvar )  (lst : eq list ) =
     List.map (subst_loc xv yv ) lst
 
    
+
+
   
       
 end;;
+
+
+
 (** Substitutes x by y  in pure formula f *)
  
 (* let subst_in_pure_f ( x : SSL_lex.locvar ) ( y : SSL_lex.locvar ) ( f : SSL_types.pure_formula ) =
