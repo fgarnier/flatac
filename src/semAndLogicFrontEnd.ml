4c65105e4ab642a2022568156240cfee93d688d3
Maxime Gaudin <gaudin.maxime@gmail.com>
2011-05-18 10:44:07 +0200

    Review code et commentaire du front-end

--- src/semAndLogicFrontEnd.ml
+++ src/semAndLogicFrontEnd.ml
@@ -1,10 +1,31 @@
+(*
+	Maxime Gaudin - VERIMAG 2011 
+
+	** THIS MODULE IS A PART OF FLATA-C, DEVELOPED AT VERIMAG (2011)
+
+	This module contains the definition of an eCFG and implements a generic algorithm to
+	fill it with the correct Abstract Interpretation and counter automata label
+
+	For any question mail us to :
+	- maxime.gaudin@imag.fr
+	- florent.garnier@imag.fr
+*)
 open Self
 open Cil_types
 
+(** This virtual class define an interface between the eCFG and the underlying user logic *)
+(** 'a type is the abstract interpretation type *)
 class virtual ['a] semAndLogicFrontEnd = 
 object 
+	(** Returns the abstract interpretation of the entry point *)
 	method virtual getEntryPointAbstraction : unit -> 'a
+
 	method virtual getEntryPointPrecondition : unit -> 'a
+
+	(** Returns true if the given state is an error state *)
 	method virtual isErrorState: 'a -> bool
+
+	(** Returns a the next couple of abstract interpretation and counter automata label based 
+	 on the current abstraction, the current counter, and the statement kind *)
 	method virtual next : 'a -> string -> stmtkind -> ('a * string)
 end
