280d6f2701a77a828c5fc7887cd8bce3e929ff2a
Florent Garnier <florent.garnier@imag.fr>
2011-05-30 18:19:07 +0200

    daily backup

--- src/SSL/ssl_substitution.ml
+++ src/SSL/ssl_substitution.ml
@@ -2,7 +2,7 @@
 as how to transfor the syntax of a SSL formula upon a substitution.*)
 
 
-open union_find
+open Union_find
 open List
 open Hashtbl
 open Ssl_types
@@ -12,6 +12,19 @@ open Ssl_types.SSL_lex
 (* Keys : Domain of the substitutionm and values are the range *)
 type loc_subst =  Subst of (locvar , locvar ) t
 
+let eq_class_inversor  (repres : SSL_lex.locvar ) (lvars : SSL_lex.locvar )
+    () (tble : (locvar , locvar) t) =
+  Hashtbl.add tble lvars repres; tble 
+
+
 let subst_from_partition (part : Union_find.partition ) =
+  let inverse_image_folder (locv : SSL_lex.locvar) (eq_c : Union_find.eqclass)
+      (table_subst : (locvar , locvar) t) =
+    (Hashtbl.fold ( eq_class_inversor eq_c.repres ) eq_c.members table_subst)
+   
+  in
+      
   match part with 
-      Partition (table_part ) ->
+      Partition (table_part ) -> Hashtbl.fold  inverse_image_folder table_part (Hashtbl.create SSL_lex.size_hash)
+
+
