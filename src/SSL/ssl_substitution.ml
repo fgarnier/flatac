4b30f5a443f98ba06bcf86571895aff6c09e164e
Florent Garnier <florent.garnier@imag.fr>
2011-05-31 13:16:57 +0200

    commiting and pushing the application of a substitution

--- src/SSL/ssl_substitution.ml
+++ src/SSL/ssl_substitution.ml
@@ -22,9 +22,29 @@ let subst_from_partition (part : Union_find.partition ) =
       (table_subst : (locvar , locvar) t) =
     (Hashtbl.fold ( eq_class_inversor eq_c.repres ) eq_c.members table_subst)
    
-  in
-      
+  in    
   match part with 
       Partition (table_part ) -> Hashtbl.fold  inverse_image_folder table_part (Hashtbl.create SSL_lex.size_hash)
 
+
+
+
+(* This fonction shall not appear in the ml-interface file *)
+
+let subst_against_affectation (subst : loc_subst )(affect_table : ((SSL_lex.ptvar , (SSL_lex.locvar , unit) t ) t)) =
+  let subst_map subst_table current_table lvar_table () =
+    if ( Hashtbl.mem substable lvar ) == true
+    then 
+	Hashtbl.remove current_table lvar;
+	Hashtbl.add current_table (Hashtbl.find substable lvar) ()
+    else ()
+  in
+  let affect_table_iterator subst_table pvar lvar_table =
+    Hashtbl.iter ( subst_map subst_table lvar_table ) lvar_table
+  in
+  match subst with 
+      Substitution ( table_subst ) ->
+	Hashtbl.iter (affect_table_iterator subst_table ) affect_table
+	
+  
 
