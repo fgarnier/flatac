527c541c0871bdefa530e6fbd0745e089e381d81
Florent Garnier <florent.garnier@imag.fr>
2011-06-21 10:46:53 +0200

    Adding var_elim to r_4 and R_6

--- src/SSL/ssl_biabduction.ml
+++ src/SSL/ssl_biabduction.ml
@@ -11,5 +11,9 @@ open Ssl_substitution
 let biabduction (etp : entail_problem ) =
   normalize_ssl etp.left;
   normalize_ssl etp.right;
+  entail_r4 etp;
+  entail_r6 etp;
+  entail_r1 etp;
+  entail_r2 etp
   
   
