6abe9dc130da9e14c443418ee0d7bdbdd3613aac
Florent Garnier <florent.garnier@imag.fr>
2011-07-06 17:15:35 +0200

    commiting changes within sem_and_logic related stuff

--- src/flatac_ssl_front_end.ml
+++ src/flatac_ssl_front_end.ml
@@ -9,15 +9,23 @@ open Cil_types
 open Sem_and_logic_front_end
 open Ssl_types
 open Cautomata
-
+open Ssl_decision
 
 
 class ssl_flatac_front_end = object (self)
   inherit [SSL_lex.formula , Cautomata.trans_label list ]  sem_and_logic_front_end
 
   method get_entry_point_abstraction () =
+    Ssl_lex.create_ssl_f ()
+
+  method is_error_state (sslf : ssl_formula ) =
+    match ssl_formula.space with
+	Top_heap -> true
+      | _ -> not (Ssl_decision.sat_ssl sslf)
     
 
+  method next (sslf :  ssl_formula ) ( labels :trans_label list ) (skind :  Cil.types.stmtkind) =
+    
     
 end
 
