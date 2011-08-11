1aac5eceacc7b2b38463965851b56885a6abf5f5
Florent Garnier <florent.garnier@imag.fr>
2011-08-11 16:48:59 +0200

    adding a nts.ml specific file

---
+++ src/flata_cautomata/nts.ml
@@ -0,0 +1,13 @@
+(*  
+This files contains the functions used to deal with Numerical Transition
+Systems, a.k.a. counter automata.
+
+_ Translating intermediate language into nts language.
+
+If you have any questions or suggestions, please
+type the following address starting from the
+rightmost caracter until you reach the leftmost
+one in your favorite mail editor : rf.gami-at-reinrag.tnerolf,
+and then send me your message.
+*)
+
