0c064fdf5b3ea8819afc102b81089902babd2ed4
Florent Garnier <florent.garnier@imag.fr>
2011-09-08 19:05:03 +0200

    thursday evening backup ...

--- src/flatac_ssl_front_end.ml
+++ src/flatac_ssl_front_end.ml
@@ -13,13 +13,14 @@ open Ssl_decision
 open Ssl_pprinters 
 open C_upon_ssl_domain (* Contains the semantic tranformation
 		       of the C instruction on the SSL formulae*)
+open Nts_types
 open Global_mem
 
 
 open Self
 
 class ssl_flatac_front_end = object 
-  inherit [SSL_lex.formula , Cautomata.trans_label list ]  sem_and_logic_front_end
+  inherit [SSL_lex.formula , Nts_types.cnt_trans_label list ]  sem_and_logic_front_end
    
 
   val mid =  (new global_mem_manager )
