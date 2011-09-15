0ec6b18efdf4977b3c876b0b2de38fc60ab86f81
Florent Garnier <florent.garnier@imag.fr>
2011-09-15 16:35:46 +0200

    plop

--- src/flatac_ssl_front_end.ml
+++ src/flatac_ssl_front_end.ml
@@ -15,12 +15,13 @@ open C_upon_ssl_domain (* Contains the semantic tranformation
 		       of the C instruction on the SSL formulae*)
 open Nts_types
 open Global_mem
-
+open Ssl_validity_absdomain (* This type defines 2-uples of ssl_formual and
+			    a validity_loc_map*)
 
 open Self
 
 class ssl_flatac_front_end = object 
-  inherit [SSL_lex.formula , Nts_types.cnt_trans_label list ]  sem_and_logic_front_end
+  inherit [ssl_validity_absdom , Nts_types.cnt_trans_label list ]  sem_and_logic_front_end
    
 
   val mid =  (new global_mem_manager )
@@ -31,35 +32,35 @@ class ssl_flatac_front_end = object
   method get_entry_point_abstraction () =
     Ssl.create_ssl_f ()
 
-  method is_error_state (sslf : ssl_formula ) =
-    match sslf.space with
+  method is_error_state (sslv : ssl_validity_absdom ) =
+    match sslv.ssl_part.space with
 	Top_heap -> true
-      | _ -> not (Ssl_decision.sat_ssl sslf)
+      | _ -> not (Ssl_decision.sat_ssl sslv.ssl_part)
 
 
-  method pretty (sslf : ssl_formula ) =
-    Ssl_pprinters.pprint_ssl_formula_tex sslf
+  method pretty (sslv : ssl_validity_absdom ) =
+    Ssl_pprinters.pprint_ssl_formula_tex sslv.ssl_part
 
     
-  method next (sslf : ssl_formula)(translist : trans_lanel list)
+  method next (sslv :ssl_validity_absdom )(translist : trans_lanel list)
     (skind : Cil_types.stmtkind) =
     let sslf_local = Ssl.copy sslf in
-    C_upon_ssl_domain.next_on_ssl mid sslf_local skind trans_list;
+    C_upon_ssl_domain.next_on_ssl mid sslf_local skind trans_list; 
     (sslf_local , ())::[]
     
 
  
   method pretty_label () = ""
     
-  method accepts sslfg sslfd =
+  method accepts sslvg sslvd =
 
     (** One checks that the current abstraction entails the next state
     abstraction, where the current state abstraction is sslfd and
     the next state abstraction is sslfg (Inverted order) *)
    
     let etp = {
-      left = sslfd ;
-      right = sslfg;
+      left = sslvd.ssl_part ;
+      right = sslvg.ssl_part;
     }
     in
     not (Ssl_entailement.does_entail etp )
