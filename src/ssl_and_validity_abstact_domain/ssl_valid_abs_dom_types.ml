99f1a5f4951489343d66c8d016c65129849447f8
Florent Garnier <florent.garnier@imag.fr>
2011-09-15 14:19:09 +0200

    ssl_valid_abs_dom_types added and compiles

--- src/ssl_and_validity_abstact_domain/ssl_valid_abs_dom_types.ml
+++ src/ssl_and_validity_abstact_domain/ssl_valid_abs_dom_types.ml
@@ -1,7 +1,7 @@
 open Var_validity_types
 open Var_validity
-open SSL_types
-open SSL
+open Ssl_types
+open SSL_lex
 
 type ssl_validity_absdom = { ssl_part : ssl_formula ;
 			     validinfos : validity_loc_map ;}
