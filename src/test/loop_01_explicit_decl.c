b9eb7dce099edf3f8c2bb15a54b0aba16324cbe6
Florent Garnier <fgarnier@imag.fr>
2011-09-26 14:57:38 +0200

    adding example with some expicit varibles declarations

---
+++ src/test/loop_01_explicit_decl.c
@@ -0,0 +1,19 @@
+#include <stdlib.h> 
+
+int main () {
+  int z = 0; // TO BE SURE
+
+  int* x = NULL;
+  int* y = NULL;
+
+  x = (int*)malloc(sizeof(int) * 1);
+  y = NULL;
+
+  while(z++ < 100) {
+    y = x;
+    x = (int*)malloc(sizeof(int) * 1);
+    free(y);
+  }
+
+  return 0;
+}
