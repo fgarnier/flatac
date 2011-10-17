d55aa4a9991658527340515faa5c65cdceb9cfef
Florent Garnier <fgarnier@imag.fr>
2011-10-17 17:53:00 +0200

    Adding missing self.ml file

---
+++ src/self.ml
@@ -0,0 +1,23 @@
+(*
+   One need to add FRAMAC- ORIGINAL COPIRIGHT NOTICE HERE, with LGPL2
+  stuff and consorts !
+
+
+	Maxime Gaudin - VERIMAG 2011 
+
+	** THIS MODULE IS A PART OF FLATA-C, DEVELOPED AT VERIMAG (2011)
+
+	This module declares the plugin into Frama-C and allow
+ other plugin to access to the logging system.
+
+	For any question mail us to :
+	- maxime.gaudin@imag.fr
+	- florent.garnier@imag.fr
+*)
+module Self =
+	Plugin.Register
+		(struct
+			let name = "FlataC"
+			let shortname = "FlataC"
+			let help= "Extracts flat counter automata based model of C program for memory faults detection."
+		end)
