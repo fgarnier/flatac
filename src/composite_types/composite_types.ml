60759fc610d0aff04766a52d8538fa8ba6963432
Florent Garnier <fgarnier@imag.fr>
2011-11-16 16:11:10 +0100

    ready to propagate validity informations into abstract domain global informations

--- src/composite_types/composite_types.ml
+++ src/composite_types/composite_types.ml
@@ -1,5 +1,28 @@
 open Composite_type_types
 
+exception CType_not_found of string
+
+let pprint_index_of_type_pointer_path (i : index_of_composite_types) =
+  let pprint_path_folder (path : string ) (ciltype : Cil_types.typ ) 
+      (prefix : string) =
+    let prefix = path^":"^( Ast_goodies.pprint_ciltypes ciltype)^"\n"^prefix
+    in prefix
+  in
+  let pprint_type_structure_folder (type_name : c_type_name )  path_table 
+      (prefix : string) =
+    match type_name  with
+        CTypeName (name_type)   ->
+	  let ret = prefix^"\n"^name_type^" : " in
+	  let ret = Hashtbl.fold pprint_path_folder 
+	    path_table ret  in
+	  ret 
+  in
+  match i with 
+      IndexCompositeTypes (index_table ) ->
+	Hashtbl.fold pprint_type_structure_folder index_table ""
+
+
+      
 
 
 let create_index_of_composite_types =
@@ -16,8 +39,19 @@ let copy_index_of_composite_types ( i : index_of_composite_types ) =
 
 let get_index_of_pointer_by_type_name (i: index_of_composite_types ) (ctype : c_type_name ) =
   match i with
-       IndexCompositeTypes (table) ->
-	 Hashtbl.find table ctype
-
+	IndexCompositeTypes (table) ->
+	  begin
+	    try
+	      Hashtbl.find table ctype
+	    with
+	      | Not_found -> 
+		begin
+		  match ctype with 
+		      CTypeName(typename) ->
+			let msg= Format.sprintf "No description for type [%s] in %s \n" typename (pprint_index_of_type_pointer_path i)
+			in 
+			raise (CType_not_found(msg))
+		end
+	  end
 
 
