4d73e168917584eba883a8b59dd035d88bf05edb
Florent Garnier <fgarnier@imag.fr>
2011-12-02 16:38:23 +0100

    plop

--- src/flata_cautomata/nts_types.ml
+++ src/flata_cautomata/nts_types.ml
@@ -15,11 +15,22 @@ exception Invalid_nts_expression
 
 (* That type describes **)
 
+type nts_base_types = NtsIntType
+		      | NtsRealType
+		      | NtsArray of string * int * nts_base_types   
+(* An array has a name, a size and contains the type of
+each element 
+This type definition allows to encode multi dimentional arrays.
+*)   			      
+ 
+			      
+
 type nts_var = NtsIVar of string (*Integer type variable*)
 	       | NtsRVar of string (*Real valued variable*)
 	       (*| NtsBVar of String (*Boolean variable*)*)
 	       | NtsMiscType of string (* Just here for the demo*)
-		   
+		
+
 
 
 type cnt_binop = CntEq
