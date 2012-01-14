3ab07a6017273451c3d068df663a669d33e44783
Florent Garnier <fgarnier@imag.fr>
2012-01-14 20:02:42 +0100

    je me casse, raz le bol de ce machin

--- src/flata_cautomata/nts_types.ml
+++ src/flata_cautomata/nts_types.ml
@@ -80,7 +80,7 @@ type cnt_bool = CntBool of cnt_binop *  cnt_arithm_exp * cnt_arithm_exp
 		| CntBOr of cnt_bool * cnt_bool
 
 type cnt_trans_label = CntGuard of cnt_bool
-		   | CntFunCall of string * nts_var option *cnt_arithm_exp list
+		   | CntFunCall of string * nts_var list option * cnt_arithm_exp list
 		   | CntAffect of nts_var * cnt_arithm_exp
 		   | CntHavoc of nts_var list (* The value of the listed 
 						 variables are not copied.
