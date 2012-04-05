f5bc3dcbd019b133d08265d7f271e798216cb2f5
Florent Garnier <fgarnier@imag.fr>
2012-04-05 16:50:24 +0200

    replacing Julien's rate limier by a version that suits flatac semantic

--- test/exemples_julienh/rate_limiter.c
+++ test/exemples_julienh/rate_limiter.c
@@ -1,18 +1,18 @@
 #include <assert.h>
 
-extern double input();
-void rate_limiter() {
-	double x_old;
-	x_old = 0;
-	while (1) {
-		assert(-1000 <= x_old && x_old <= 1000);
-		double x = input();
-		if (x <= -1000 || x >= 1000) x=0;
-		if (x >= x_old+1)
-			x = x_old+1;
-		if (x <= x_old-1)
-			x = x_old-1;
-		x_old = x;
-	}
+int __NdetValInRange(int,int);
+
+
+int main (int argc, char ** argv) {
+  int x_old = 0;
+  while (1) {
+    int x = __NdetValInRange(-100, 100);
+    if (x > x_old+10) x = x_old+10;
+    if (x < x_old-10) x = x_old-10;
+    x_old = x;
+    assert(x <= 100 && x>= -100);
+ } 
+
 }
+
 
